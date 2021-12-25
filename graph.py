71207329c06f52b9e52495cade17fe3d51bf4f12
miquelperello <miquelperello@github.com>
2021-12-25 15:18:23 +0100

    .

--- graph.py
+++ graph.py
@@ -8,10 +8,10 @@ llistaComparativa = [df.PerdAvio, df.TempsCua,
 NomComparativa = ['Passatgers que perden avió', 'Temps en fer la cua',
                   'Temps al mostrador', 'Temps desde que entren a la cua fins la sortida']
 
+
 for item in range(0, len(llistaComparativa)):
 
     plt.scatter(x=df.Passatgers, y=llistaComparativa[item])
-
     plt.ylabel(NomComparativa[item])
     plt.xlabel("Passatgers")
     plt.show()
